678a58564e21def1f9385c1d0eea20268169041d
reizist <reizist@gmail.com>
2018-10-04 00:28:19 +0900

    Implement help flag

--- q3/curl.go
+++ q3/curl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -12,6 +13,7 @@ type Options struct {
 	method    string
 	headers   string
 	isVerbose bool
+	isHelp    bool
 	url       string
 }
 
@@ -20,14 +22,21 @@ func ParseOptions() *Options {
 	method := flag.String("X", "GET", "Specify request command to use")
 	headers := flag.String("H", "", "Pass custom header LINE to server (H)")
 	isVerbose := flag.Bool("v", false, "Make the operation more talkative")
+	isHelp := flag.Bool("h", false, "This help text")
+	flag.Usage = usage
 	flag.Parse()
 
-	url := flag.Args()[0]
+	if len(flag.Args()) < 1 {
+		fmt.Println("try 'curl -h' for more information")
+		os.Exit(2)
+	}
 
+	url := flag.Args()[0]
 	options.url = url
 	options.method = *method
 	options.headers = *headers
 	options.isVerbose = *isVerbose
+	options.isHelp = *isHelp
 
 	return options
 }
@@ -48,6 +57,11 @@ func createRequest(options Options) *http.Request {
 func main() {
 	options := ParseOptions()
 
+	if options.isHelp {
+		usage()
+		os.Exit(1)
+	}
+
 	request := createRequest(*options)
 	client := &http.Client{}
 	resp, err := client.Do(request)
@@ -66,4 +80,23 @@ func main() {
 		body, _ := ioutil.ReadAll(resp.Body)
 		fmt.Printf("%q\n", body)
 	}
+}
+
+var usageinfo string = `curl is a Go implemented CLI cURL-like tool for humans.
+Usage:
+	curl [flags] URL
+flags:
+  -v: Make the operation more talkative
+METHOD:
+  defaults to either GET (if there is no specified method)
+URL:
+  The only information needed to perform a request is a URL.
+Example:
+	curl https://example.com
+more help information please refer to https://github.com/reizist/treasure/q3
+`
+
+func usage() {
+	fmt.Println(usageinfo)
+	os.Exit(2)
 }
